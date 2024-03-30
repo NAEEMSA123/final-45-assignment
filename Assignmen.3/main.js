77b0d1ecca31ebd4619080f294b4b408b610c374
Naeem Ahmed <naeemsahito890629@gmail.com>
2024-03-30 06:34:14 -0700

    first commit

---
+++ Assignmen.3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing the famous quote and its author
+let quote = "A candle losese nothing of its light by lighting an other a candle";
+let auther = "naeem ahmed";
+//printing the message
+console.log(`"${quote}" - ${auther}`);
---
+++ Assignmen.3/main.ts
@@ -0,0 +1,9 @@
+// storing the famous quote and its author
+
+let quote: string = "A candle losese nothing of its light by lighting an other a candle";
+
+let auther:string = "naeem ahmed";
+
+//printing the message
+
+console.log(`"${quote}" - ${auther}`);
